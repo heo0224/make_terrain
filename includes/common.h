2d5d6986040c01895fbe3c64d8e48f820556b9e7
donggeunlim <rms0329@gmail.com>
2024-11-23 13:19:22 +0900

    feat: add initial build setup

---
+++ includes/common.h
@@ -0,0 +1,14 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include <vector>
+#include <string>
+#include <optional>
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+#include <spdlog/spdlog.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#endif // __COMMON_H__
