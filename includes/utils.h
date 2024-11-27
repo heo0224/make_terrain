7bb1dd4a65121a68076157afc400348350d1502a
donggeunlim <rms0329@gmail.com>
2024-11-27 14:05:17 +0900

    feat: add overloaded util functions

--- includes/utils.h
+++ includes/utils.h
@@ -4,8 +4,11 @@
 #include "common.h"
 
 unsigned int generatePositionVAO(const float* vertices, unsigned int vertexSize);
+unsigned int generatePositionVAO(const std::vector<float>& vertices);
 unsigned int generatePositionTextureVAO(const float* vertices, unsigned int vertexSize);
+unsigned int generatePositionTextureVAO(const std::vector<float>& vertices);
 unsigned int generatePositionTextureVAOWithEBO(const float* vertices, unsigned int vertexSize, const unsigned int* indices, unsigned int indexSize);
+unsigned int generatePositionTextureVAOWithEBO(const std::vector<float>& vertices, const std::vector<unsigned int>& indices);
 
 
 
