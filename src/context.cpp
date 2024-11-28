5ec151a5371adda8671f3d725f20b634f91cf4cb
donggeunlim <rms0329@gmail.com>
2024-11-28 22:52:54 +0900

    chore: refactor and clean

--- src/context.cpp
+++ src/context.cpp
@@ -1,6 +1,6 @@
 #include "context.h"
-#include "geometry_primitives.h"
 #include "utils.h"
+#include "geometry_primitives.h"
 #include <imgui.h>
 
 std::unique_ptr<Context> Context::create() {
@@ -12,30 +12,9 @@ std::unique_ptr<Context> Context::create() {
 
 bool Context::init() {
     camera = std::make_unique<Camera>();
-    sun = std::make_shared<DirectionalLight>(30.0f, 30.0f, glm::vec3(0.8f));
-    
-    shader = std::make_unique<Shader>("../shaders/shader.vs", "../shaders/shader.fs");
-    skyboxShader = std::make_unique<Shader>("../shaders/shader_skybox.vs", "../shaders/shader_skybox.fs");
-    waterShader = std::make_unique<Shader>("../shaders/shader_water.vs", "../shaders/shader_water.fs");
-    containerTexture = std::make_shared<Texture>("../assets/container.jpg");
-    grassGroundtexture = std::make_shared<Texture>("../assets/grass_ground.jpg");
-    skyboxTexture = std::make_shared<CubemapTexture>(
-        std::vector<std::string> {
-        "../assets/skybox/right.tga",
-            "../assets/skybox/left.tga",
-            "../assets/skybox/top.tga",
-            "../assets/skybox/bottom.tga",
-            "../assets/skybox/front.tga",
-            "../assets/skybox/back.tga"
-    }
-    );
-
+    skybox = std::make_unique<Skybox>(this);
     terrain = Terrain::createWithTessellation(this);
-    skyBoxVAO = generatePositionVAO(skyBoxPositions, sizeof(skyBoxPositions));
-    cubeVAO = generatePositionTextureVAO(cubePositionsTextures, sizeof(cubePositionsTextures));
-    quadVAO = generatePositionTextureVAOWithEBO(quadPositionTextures, sizeof(quadPositionTextures), quadIndices, sizeof(quadIndices));
 
-    glClearColor(clearColor.r, clearColor.g, clearColor.b, clearColor.a);
     glEnable(GL_DEPTH_TEST);
 
     glGenFramebuffers(1, &FBO);
@@ -122,18 +101,7 @@ void Context::render() {
     camera->invertPitch();
 
     terrain->render();
-
-    // skybox
-    glDepthFunc(GL_LEQUAL);
-    glBindVertexArray(skyBoxVAO);
-    glm::mat4 view = glm::mat4(glm::mat3(camera->getViewMatrix()));
-    glm::mat4 projection = getProjectionMatrix();
-    skyboxShader->use();
-    skyboxShader->setMat4("view", view);
-    skyboxShader->setMat4("projection", projection);
-    skyboxShader->bindCubemapTexture("skyboxTexture1", skyboxTexture.get());
-    glDrawArrays(GL_TRIANGLES, 0, 36);
-    glDepthFunc(GL_LESS);
+    skybox->render();
 }
 
 void Context::renderGUI() {
@@ -160,10 +128,6 @@ void Context::renderGUI() {
             ImGui::SliderInt("max tess level", &terrain->maxTessLevel, terrain->minTessLevel + 1, 64);
             ImGui::SliderFloat("min distance", &terrain->minDistance, 1.0f, terrain->maxDistance);
             ImGui::SliderFloat("max distance", &terrain->maxDistance, terrain->minDistance, 100.0f);
-        }
-
-        if (ImGui::CollapsingHeader("Water")) {
-            ImGui::SliderFloat("water height", &waterHeight, -1.0f, 1.0f);
         }
     }
     ImGui::End();
