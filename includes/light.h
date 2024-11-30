0cc716394b145f7842e7a0ad42831160543af09d
donggeunlim <rms0329@gmail.com>
2024-11-30 11:41:48 +0900

    chore: minor changes

--- includes/light.h
+++ includes/light.h
@@ -16,7 +16,9 @@ public:
     float azimuth = 30.0f;
     float elevation = 30.0f;
     float lightDistance = 30.0f;
-    float frustumSize = 1000.0f;
+    float frustumSize = 20.0f;
+    float nearPlane = 0.1f;
+    float farPlane = 1000.0f;
     glm::vec3 color = glm::vec3(1.0f);
     glm::vec3 direction = glm::vec3(0.0f, 0.0f, 1.0f);
 private:
