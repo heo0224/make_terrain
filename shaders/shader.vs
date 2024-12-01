bfb18052a99034efeb97b154fe136d0eb8dd1047
heo0224 <heo0224@snu.ac.kr>
2024-12-01 12:29:11 +0900

    water temp

--- shaders/shader.vs
+++ shaders/shader.vs
@@ -7,9 +7,11 @@ out vec2 TexCoord;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
-
+uniform vec4 clipPlane;
 void main()
 {
 	gl_Position = projection * view * model * vec4(aPos, 1.0f);
+	vec4 worldPosition = model * vec4(aPos, 1.0);
+    gl_ClipDistance[0] = dot(worldPosition, clipPlane);
 	TexCoord = vec2(aTexCoord.x, aTexCoord.y);
 }
--- shaders/shader_water.fs
+++ shaders/shader_water.fs
@@ -6,13 +6,20 @@ in Data{
     vec3 Normal;
     vec3 toCamera;
     vec3 fromLight;
-    vec3 Pos;
+    vec4 Pos;
 } In;
 
 uniform sampler2D texture0;
 
 void main()
 {
-    // FragColor = texture(texture0, TexCoord);
-    FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+    vec2 ndc = (In.Pos.xy / In.Pos.w) * 0.5 + 0.5;
+    // vec2 reflectionTexCoord = In.TexCoord;
+    vec2 reflectionTexCoord = vec2(ndc.x, 1.0 - ndc.y);
+    // reflectionTexCoord.x = clamp(reflectionTexCoord.x, 0.001, 0.999);
+    // reflectionTexCoord.y = clamp(reflectionTexCoord.y, -0.999, -0.001);
+
+    vec4 reflectionColor = texture(texture0, reflectionTexCoord);
+    FragColor = reflectionColor;
+    // FragColor = vec4(0.0, 0.0, 1.0, 1.0);
 }
