8ff9b4a3bd71620ba202521fa9b0c6969d4e36ba
heo0224 <heo0224@snu.ac.kr>
2024-12-02 13:07:27 +0900

    water class added

--- shaders/shader_water.fs
+++ shaders/shader_water.fs
@@ -9,17 +9,19 @@ in Data{
     vec4 Pos;
 } In;
 
-uniform sampler2D texture0;
-
+uniform sampler2D reflectionTexture;
+uniform sampler2D refractionTexture;
+uniform float mixfactor;
 void main()
 {
     vec2 ndc = (In.Pos.xy / In.Pos.w) * 0.5 + 0.5;
-    // vec2 reflectionTexCoord = In.TexCoord;
-    vec2 reflectionTexCoord = vec2(ndc.x, 1.0 - ndc.y);
-    // reflectionTexCoord.x = clamp(reflectionTexCoord.x, 0.001, 0.999);
-    // reflectionTexCoord.y = clamp(reflectionTexCoord.y, -0.999, -0.001);
+    vec2 reflectionTexCoord = vec2(ndc.x, -ndc.y);
+    vec2 refractionTexCoord = vec2(ndc.x, ndc.y);
 
-    vec4 reflectionColor = texture(texture0, reflectionTexCoord);
-    FragColor = reflectionColor;
-    // FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+    vec4 reflectionColor = texture(reflectionTexture, reflectionTexCoord);
+    vec4 refractionColor = texture(refractionTexture, refractionTexCoord);
+    // Debug individual textures (optional)
+    // FragColor = reflectionColor; return; // Test reflection
+    // FragColor = refractionColor; return; // Test refraction
+    FragColor = mix(reflectionColor, refractionColor, mixfactor);
 }
